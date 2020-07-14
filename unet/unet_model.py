d9bad2678aac37814ba151c3c06243c2daf138d0
leeneil <lishantreasure@gmail.com>
2020-07-14 14:59:19 -0700

    unet module by @milesial

---
+++ unet/unet_model.py
@@ -0,0 +1,38 @@
+"""
+Full assembly of the parts to form the complete network
+https://github.com/milesial/Pytorch-UNet/blob/master/unet/unet_model.py
+"""
+
+import torch.nn.functional as F
+from .unet_parts import *
+
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, n_filters=64, bilinear=True):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+        self.inc = DoubleConv(n_channels, n_filters)
+        self.down1 = Down(n_filters, n_filters*2)
+        self.down2 = Down(n_filters*2, n_filters*4)
+        self.down3 = Down(n_filters*4, n_filters*8)
+        self.down4 = Down(n_filters*8, n_filters*8)
+        self.up1 = Up(n_filters*16, n_filters*4, bilinear)
+        self.up2 = Up(n_filters*8, n_filters*2, bilinear)
+        self.up3 = Up(n_filters*4, n_filters, bilinear)
+        self.up4 = Up(n_filters*2, n_filters, bilinear)
+        self.outc = OutConv(n_filters, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
