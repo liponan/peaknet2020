8bb8428db9b2ae9bdc441eb663b5cd63564551fa
LCLSIT02 <axel.levy@polytechnique.edu>
2021-05-06 21:24:36 -0700

    m

--- peaknet/visualize.py
+++ peaknet/visualize.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 def scalar_metrics(writer, metrics, total_steps):
     for (key, value) in metrics.items():
@@ -10,6 +11,6 @@ def show_GT_image(writer, img_vis, target_vis, total_steps, n=5):
     for i in range(n):
         panel_name = 'panel_'+str(i)
         # writer.add_image(panel_name, img_vis[i][None, :, :], global_step=total_steps)
-        fig = matplotlib.pyplot.figure()
-        matplotlib.pyplot.imshow(img_vis[i])
+        fig = plt.figure()
+        plt.imshow(img_vis[i])
         writer.add_image(panel_name, fig, global_step=total_steps)
