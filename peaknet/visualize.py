8de95ab2fe25f8743843bdeef5e1eef095e67167
LCLSIT02 <axel.levy@polytechnique.edu>
2021-05-06 21:13:36 -0700

    m

--- peaknet/visualize.py
+++ peaknet/visualize.py
@@ -5,8 +5,10 @@ def scalar_metrics(writer, metrics, total_steps):
     for (key, value) in metrics.items():
         writer.add_scalar(key, value, total_steps)
 
-def show_GT_image(writer, img_vis, target_vis, total_steps):
-    n = img_vis.shape[0]
+def show_GT_image(writer, img_vis, target_vis, total_steps, n=5):
     for i in range(n):
-        panel_name = 'panel'+str(i)
-        writer.add_image(panel_name, img_vis[i][None, :, :], global_step=total_steps)
+        panel_name = 'panel_'+str(i)
+        # writer.add_image(panel_name, img_vis[i][None, :, :], global_step=total_steps)
+        fig = plt.figure()
+        plt.imshow(img_vis[i])
+        writer.add_image(panel_name, fig, global_step=total_steps)
