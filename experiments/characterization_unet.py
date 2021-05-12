0308415d3987ed610e3465051030d5830ad7a7d1
LCLSIT02 <axel.levy@polytechnique.edu>
2021-05-12 16:44:54 -0700

    m

--- experiments/characterization_unet.py
+++ experiments/characterization_unet.py
@@ -6,7 +6,9 @@ import os
 n_experiments = 1
 pos_weight_list = np.logspace(-2, 0, 5)
 
+os.system('cd ../peaknet')
+
 for i, pw in enumerate(pos_weight_list):
     print("Experiment #"+str(i + 1))
     print("pos_weight: "+str(pw))
-    os.system('python ../peaknet/train.py --params ../peaknet/params.json -g 0 --pos_weight '+str(pw))
+    os.system('python train.py params.json -g 0 --pos_weight '+str(pw))
