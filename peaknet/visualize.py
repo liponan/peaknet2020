8062df96cab311270cc78e9dde2f35ff6d8b28ab
LCLSIT02 <axel.levy@polytechnique.edu>
2021-05-25 11:59:02 -0700

    m

--- peaknet/visualize.py
+++ peaknet/visualize.py
@@ -7,7 +7,7 @@ def scalar_metrics(writer, metrics, total_steps):
     for (key, value) in metrics.items():
         writer.add_scalar(key, value, total_steps)
 
-def show_GT_prediction_image(writer, img_vis, target_vis, total_steps, params, device, model, n=5):
+def show_GT_prediction_image(writer, img_vis, target_vis, total_steps, params, device, model, n=32):
     for i in range(n):
         panel_name = 'panel_'+str(i)
 
@@ -18,9 +18,7 @@ def show_GT_prediction_image(writer, img_vis, target_vis, total_steps, params, d
         plt.yticks([])
 
         # Prediction
-        h = img_vis.shape[1]
-        w = img_vis.shape[2]
-        x = img_vis.view(-1, 1, h, w).to(device)  # each panel is treated independently !!0
+        x = img_vis.to(device)
         scores = model(x)
         scores = nn.Sigmoid()(scores).cpu().numpy()
         indices_nonzero = np.array(np.argwhere(scores[i, 0] > params["cutoff"]))
@@ -37,20 +35,20 @@ def show_GT_prediction_image(writer, img_vis, target_vis, total_steps, params, d
         else:
             print("Unrecognized number of classes for visualization.")
 
-            # GT
-            indices_nonzero = np.array(np.nonzero(target_vis[i, 0]))
-            if params["n_classes"] == 3:
-                shift_u = target_vis[i, 1, indices_nonzero[:, 0], indices_nonzero[:, 1]].numpy()
-                shift_v = target_vis[i, 2, indices_nonzero[:, 0], indices_nonzero[:, 1]].numpy()
-                plt.plot(indices_nonzero[:, 1] - .5 + shift_v,
-                         indices_nonzero[:, 0] - .5 + shift_u,
-                         'gs', markerfacecolor='none', markersize=10, markeredgewidth=2.0, alpha=.8)
-            elif params["n_classes"] == 1:
-                plt.plot(indices_nonzero[:, 1] - .5,
-                         indices_nonzero[:, 0] - .5,
-                         'gs', markerfacecolor='none', markersize=10, markeredgewidth=2.0, alpha=.8)
-            else:
-                print("Unrecognized number of classes for visualization.")
+        # GT
+        indices_nonzero = np.array(np.nonzero(target_vis[i, 0]))
+        if params["n_classes"] == 3:
+            shift_u = target_vis[i, 1, indices_nonzero[:, 0], indices_nonzero[:, 1]].numpy()
+            shift_v = target_vis[i, 2, indices_nonzero[:, 0], indices_nonzero[:, 1]].numpy()
+            plt.plot(indices_nonzero[:, 1] - .5 + shift_v,
+                     indices_nonzero[:, 0] - .5 + shift_u,
+                     'gs', markerfacecolor='none', markersize=10, markeredgewidth=2.0, alpha=.8)
+        elif params["n_classes"] == 1:
+            plt.plot(indices_nonzero[:, 1] - .5,
+                     indices_nonzero[:, 0] - .5,
+                     'gs', markerfacecolor='none', markersize=10, markeredgewidth=2.0, alpha=.8)
+        else:
+            print("Unrecognized number of classes for visualization.")
 
         plt.subplot(212)
         plt.imshow(img_vis[i], cmap='Blues')
