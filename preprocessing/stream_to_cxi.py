fc8163ad3d677df472901468ec45aa6f51ba6087
LCLSIT02 <axel.levy@polytechnique.edu>
2021-06-04 11:09:09 -0700

    m

--- preprocessing/stream_to_cxi.py
+++ preprocessing/stream_to_cxi.py
@@ -17,14 +17,30 @@ def get_nPeaks(extract, idx_stream):
     nPeaks = int(line.split()[2])
     return nPeaks
 
+def get_fs_ss(extract, idx_stream, nPeaks):
+    peak_pos_0 = extract.label.index[idx_stream][3] + 1
+    fs_list = []
+    ss_list = []
+    for k in range(nPeaks):
+        peak_pos = peak_pos_0 + k
+        line = extract.content[peak_pos]
+        fs = float(line.split()[0])
+        ss = float(line.split()[1])
+        fs_list.append(fs)
+        ss_list.append(ss)
+    return fs_list, ss_list
+
+
 def parse_args():
     p = argparse.ArgumentParser(description=__doc__)
     p.add_argument("--filename", "-f", type=str, required=True, help="Path to .stream file")
     p.add_argument("--events_per_cxi", type=int, default=10, help="Number of events per .cxi file")
+    p.add_argument("--max_n_peaks", type=int, default=2048, help="Maximum number of peaks")
     return p.parse_args()
 
 def main():
     args = parse_args()
+    max_n_peaks = args.max_n_peaks
 
     print("The detector is unknown.")
 
@@ -54,13 +70,15 @@ def main():
     for idx_cxi in range(n_cxi_files):
         print()
         print("Writing cxi file " + str(idx_cxi + 1) + "/" + str(n_cxi_files) + "...")
-        name_cxi = 'cxi_' + str(idx_cxi) + '.cxi'
+        name_cxi = 'file_' + str(idx_cxi) + '.cxi'
         print("Name: " + name_cxi)
         cxi_file = h5py.File(save_dir + '/' +name_cxi, 'w')
 
         event_numbers = []
         LCLS = cxi_file.create_group('LCLS')
         nPeaks_list = []
+        peak2 = np.array((args.events_per_cxi, max_n_peaks), dtype=float)
+        peak1 = np.array((args.events_per_cxi, max_n_peaks), dtype=float)
         result_1 = cxi_file.create_group('entry_1/result_1')
 
         for idx_list in range(args.events_per_cxi):
@@ -70,16 +88,26 @@ def main():
             event_number = get_event_number(extract, idx_stream)
             event_numbers.append(event_number)
             nPeaks = get_nPeaks(extract, idx_stream)
+            if nPeaks > max_n_peaks:
+                nPeaks = max_n_peaks
             nPeaks_list.append(nPeaks)
+            fs_list, ss_list = get_fs_ss(extract, idx_stream, nPeaks)
+            fs_array = np.zeros((max_n_peaks), dtype=float)
+            fs_array[:nPeaks] = np.array(fs_list)
+            ss_array = np.zeros((max_n_peaks), dtype=float)
+            ss_array[:nPeaks] = np.array(ss_list)
+            peak2[idx_list] = fs_array
+            peak1[idx_list] = ss_array
+            #
         event_numbers = np.array(event_numbers)
         LCLS.create_dataset('eventNumber', data=event_numbers)
         nPeaks_list = np.array(nPeaks_list)
         result_1.create_dataset('nPeaks', data=nPeaks_list)
+        result_1.create_dataset('peak2', data=peak2)
+        result_1.create_dataset('peak1', data=peak1)
         cxi_file.close()
-        print("eventNumber:")
-        print(event_numbers)
-        print("nPeaks")
-        print(nPeaks_list)
+        print("peak2:")
+        print(peak2)
         print(name_cxi + " closed.")
 
     print("Done with preprocessing.")
