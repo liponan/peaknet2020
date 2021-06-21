7ca25d244556e0284b415492116bbe40f73d5ef7
LCLSIT02 <axel.levy@polytechnique.edu>
2021-06-21 16:28:32 +0200

    m

--- experiments/characterization_peaknet.py
+++ experiments/characterization_peaknet.py
@@ -5,7 +5,7 @@ os.chdir("/cds/home/a/axlevy/peaknet2020/peaknet")
 
 index_experiment = 1
 
-# Experiment #1
+# Experiment #1: peaknet and unet vs pos_weight
 if index_experiment == 1:
     pos_weight_list = [1, 1e-1, 1e-2]
 
@@ -18,7 +18,7 @@ if index_experiment == 1:
         print("---")
         save_name = prefix + str(offset_idx + i)
         os.system('python train.py params_model_1.json --saver_type "precision_recall"'
-                  ' --n_experiments 1 --n_per_run 1 --n_epochs 1--save_name ' + str(save_name) + ' --pos_weight ' + str(pw))
+                  ' --n_experiments 1 --n_per_run 1 --n_epochs 1 --save_name ' + str(save_name) + ' --pos_weight ' + str(pw))
 
     offset_idx = 0
     prefix = "pos_weight_unet_"
@@ -29,5 +29,5 @@ if index_experiment == 1:
         print("---")
         save_name = prefix + str(offset_idx + i)
         os.system('python train.py params.json --saver_type "precision_recall"'
-                  ' --n_experiments 1 --n_per_run 1 --n_epochs 1--save_name ' + str(save_name) + ' --pos_weight ' + str(
+                  ' --n_experiments 1 --n_per_run 1 --n_epochs 1 --save_name ' + str(save_name) + ' --pos_weight ' + str(
             pw))
