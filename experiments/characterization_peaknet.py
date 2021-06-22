0bb179253c67271715a2681e5649e11321fc1c71
LCLSIT02 <axel.levy@polytechnique.edu>
2021-06-22 16:31:44 +0200

    m

--- experiments/characterization_peaknet.py
+++ experiments/characterization_peaknet.py
@@ -3,7 +3,7 @@ import os
 
 os.chdir("/cds/home/a/axlevy/peaknet2020/peaknet")
 
-index_experiment = 4
+index_experiment = 5
 
 # Experiment #1: peaknet and unet vs pos_weight
 if index_experiment == 1:
@@ -78,4 +78,20 @@ if index_experiment == 4:
         os.system('python train.py params_model_1.json --saver_type "precision_recall"'
                   ' --n_experiments -1 --n_per_run -1 --n_epochs 1 --show_image_every 10000'
                   ' --use_indexed_peaks True --use_adaptive_filtering False'
+                  ' --save_name ' + str(save_name) + ' --pos_weight ' + str(pw))
+
+# Experiment #5: BCE vs FL on PeakNet with indexing and AF
+if index_experiment == 5:
+    pos_weight_list = [1, 1e-1, 1e-2]
+    offset_idx = 0
+    prefix = "pos_weight_FL_peaknet_"
+    for i, pw in enumerate(pos_weight_list):
+        print("---")
+        print("Experiment #" + str(i + 1))
+        print("pos_weight: " + str(pw))
+        print("---")
+        save_name = prefix + str(offset_idx + i)
+        os.system('python train.py params_model_1.json --saver_type "precision_recall"'
+                  ' --n_experiments -1 --n_per_run -1 --n_epochs 1 --show_image_every 1000'
+                  ' --use_indexed_peaks True --use_adaptive_filtering True --use_focal_loss True'
                   ' --save_name ' + str(save_name) + ' --pos_weight ' + str(pw))
